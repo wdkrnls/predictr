7765029892fdde4b1258a3a6d262f93a85ee58ea
Kyle Andrews <kyle.c.andrews@gmail.com>
2018-03-27 23:23:47 -0400

    Added at higher order function.
    This could be a helper function for choosing "identity" statistics.

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,12 @@
 #' Test whether a string has been transformed.
+#' @param x Character.
 is_transformed <- function(x) grepl(pattern = "^.+\\(.+\\)$", x)
 
 
 #' Capture a single group from a string.
+#' @param x Character.
+#' @param pattern Character Scalar.
+#' @return Character Scalar.
 #' @importFrom utils strcapture
 capture <- function(x, pattern) {
   unlist(strcapture(pattern, x, proto = ""),
@@ -11,6 +15,7 @@ capture <- function(x, pattern) {
 
 
 #' Extract the transformation function in the variable.
+#' @param x Character.
 trans_function <- function(x) {
   z <- is_transformed(x)
   ifelse(z, capture(x, "^(.+)\\(.+\\)$"), "identity")
@@ -18,8 +23,22 @@ trans_function <- function(x) {
 
 
 #' Extract the (potentially) transformed variable
+#' @param x Character.
+#' @return Character.
 trans_variable <- function(x) {
   z <- is_transformed(x)
   ifelse(z, capture(x, "^.+\\((.+)\\)$"), x)
 }
 
+
+#' Return a specific value, no matter the level of x.
+#'
+#' This is supposed to be helpful for choosing specific values for
+#' your statistic.
+#' @param y Numeric value to pick.
+#' @return Function with parameter x.
+#' @export
+at <- function(y) {
+  stopifnot(length(y) == 1)
+  function(x) y
+}
